949f32f52ca2afb5019cfbde177475f4ac388051
Alexandre MONSCHEIN <a.monschein@lyon.ort.asso.fr>
2020-12-02 11:24:59 +0100

    add controllers, services, repositories...

---
+++ src/main/java/com/ort/mediconsent/controllers/rest/UtilisateurController.java
@@ -0,0 +1,4 @@
+package com.ort.mediconsent.controllers.rest;
+
+public class UtilisateurController {
+}
---
+++ src/main/java/com/ort/mediconsent/services/UtilisateurServices.java
@@ -0,0 +1,4 @@
+package com.ort.mediconsent.services;
+
+public class UtilisateurServices {
+}
