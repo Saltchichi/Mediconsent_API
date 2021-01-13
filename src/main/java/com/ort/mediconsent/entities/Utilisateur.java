fe6bb6e36240973965a8635b68a8adcc42207c03
Saltchichi <a.monschein@lyon.ort.asso.fr>
2021-01-13 08:30:46 +0100

    user services begin

--- src/main/java/com/ort/mediconsent/entities/Utilisateur.java
+++ src/main/java/com/ort/mediconsent/entities/Utilisateur.java
@@ -23,6 +23,46 @@ public class Utilisateur {
     @Column(name = "numero_securite_sociale")
     private Long numero_securite_sociale;
 
+    public Long getId_utilisateur() {
+        return id_utilisateur;
+    }
+
+    public void setId_utilisateur(Long id_utilisateur) {
+        this.id_utilisateur = id_utilisateur;
+    }
+
+    public String getNom_utilisateur() {
+        return nom_utilisateur;
+    }
+
+    public void setNom_utilisateur(String nom_utilisateur) {
+        this.nom_utilisateur = nom_utilisateur;
+    }
+
+    public String getPrenom_utilisateur() {
+        return prenom_utilisateur;
+    }
+
+    public void setPrenom_utilisateur(String prenom_utilisateur) {
+        this.prenom_utilisateur = prenom_utilisateur;
+    }
+
+    public String getMot_de_passe_utilisateur() {
+        return mot_de_passe_utilisateur;
+    }
+
+    public void setMot_de_passe_utilisateur(String mot_de_passe_utilisateur) {
+        this.mot_de_passe_utilisateur = mot_de_passe_utilisateur;
+    }
+
+    public Long getNumero_securite_sociale() {
+        return numero_securite_sociale;
+    }
+
+    public void setNumero_securite_sociale(Long numero_securite_sociale) {
+        this.numero_securite_sociale = numero_securite_sociale;
+    }
+
     public Utilisateur() {
     }
 
--- src/main/java/com/ort/mediconsent/services/UtilisateurServices.java
+++ src/main/java/com/ort/mediconsent/services/UtilisateurServices.java
@@ -1,8 +1,11 @@
 package com.ort.mediconsent.services;
 
 import com.ort.mediconsent.entities.Utilisateur;
+import com.sun.crypto.provider.PBKDF2HmacSHA1Factory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.DigestUtils;
 
+import javax.crypto.spec.PBEKeySpec;
 import java.util.List;
 
 public class UtilisateurServices {
@@ -13,23 +16,13 @@ public class UtilisateurServices {
         return UtilisateurRepository.findAll();
     }
 
-   /* public Utilisateur editUser(Utilisateur user) {
-        Utilisateur dbUser = getUserById(user.getLogin());
-        dbUser.setNom(user.getNom());
-        dbUser.setPrenom(user.getPrenom());
-        if (user.getMotDePasse() != null) {
-            dbUser.setMotDePasse(DigestUtils.md5Hex(user.getMotDePasse()));
-        }
-        return UtilisateurRepository.save(dbUser);
-    }*/
-
-    /*public Utilisateur saveUser(Utilisateur user)
-    {
-        if (user.getMotDePasse() != null) {
-            user.setMotDePasse(DigestUtils.md5Hex(user.getMotDePasse()));
+    public Utilisateur saveUser(Utilisateur user) {
+        if (user.getMot_de_passe_utilisateur() != null) {
+            //TODO set the encrypted user password
+            /*user.setMot_de_passe_utilisateur(DigestUtils.md5Hex(user.getMot_de_passe_utilisateur()));*/
         }
         return UtilisateurRepository.save(user);
-    }*/
+    }
 
     public Utilisateur getUserById(Long id) {
         return UtilisateurRepository.findById(id).orElse(null);
@@ -39,9 +32,9 @@ public class UtilisateurServices {
         UtilisateurRepository.save(new Utilisateur(nom_utilisateur, prenom_utilisateur));
     }
 
-    /*public void addUser(Utilisateur user) {
-        addUser(user.getNom(), user.getPrenom());
-    }*/
+    public void addUser(Utilisateur user) {
+        addUser(user.getNom_utilisateur(), user.getPrenom_utilisateur());
+    }
 
     public void removeUser(Long id) {
         UtilisateurRepository.deleteById(id);
